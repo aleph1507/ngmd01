88e23d551b2f2be06bf894158fe9c4d30e848e59
aleph1507 <xrristo@gmail.com>
2018-04-11 16:38:26 +0200

    form1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,31 @@ import { NgModule } from '@angular/core';
 
 import 'hammerjs';
 
+import { MatDialogModule } from '@angular/material';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatButtonModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { MyFormComponent } from './my-form/my-form.component';
+import { DialogDemoComponent } from './dialog-demo/dialog-demo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFormComponent,
+    DialogDemoComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
